5c1682870a42285ec5de6db9220d88bf6b5d30d4
Olena_Oliinyk <elenikqa@ukr.net>
2019-07-23 13:11:33 +0300

    before after and mainTest

---
+++ src/test/java/OlenaProject/testPac/MainPageTest.java
@@ -0,0 +1,12 @@
+package OlenaProject.testPac;
+
+import org.junit.Test;
+
+public class MainPageTest  extends ParentTest{
+    public MainPageTest(){}
+
+    @Test
+    public void projectTest(){
+
+    }
+}
--- src/test/java/OlenaProject/testPac/ParentTest.java
+++ src/test/java/OlenaProject/testPac/ParentTest.java
@@ -1,8 +1,42 @@
 package OlenaProject.testPac;
 
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 public class ParentTest {
+    public WebDriver webDriver;
+    Logger logger = Logger.getLogger(getClass());
+
     public ParentTest() {
     }
 
+    @Before
+    public void setUp() {
 
+        File driverPath = new File("../OlenaOliiynykProj/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", driverPath.getAbsolutePath());
+
+
+        webDriver = new ChromeDriver();
+        //webDriver = new EdgeDriver();
+
+        //mainUrl = new MainUrl(webDriver);
+        // practice = new Practice(webDriver);
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+    }
+
+    @After
+    public void tearDown() {
+        if (!(webDriver == null)) {
+            webDriver.quit();
+        }
+    }
 }
